af02688f7e594d839ecdf2584724526fcd220371
frosty <financeFrosty@yandex.ru>
2022-03-29 22:19:03 +0300

    first commit, fixed strictPropertyInitialization in tsconfig.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: true },
+    { description: 'laugh', done: true },
+  ];
+
+  get items(){
+    if (this.filter === 'all'){
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string){
+    this.allItems.unshift({
+      description,
+      done: false
+    })
+  }
+
+  remove(item: any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
